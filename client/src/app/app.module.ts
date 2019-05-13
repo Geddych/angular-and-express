8a11afc0e7f2d0506f6fa839e1448ad7ebc0f4c4
Dmitry Trukhin <geddych@gmail.com>
2019-05-13 21:19:50 +0500

    frontend

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './shared/services/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    RegisterPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/shared/services/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap} from 'rxjs/operators'
+import { tokenReference } from '@angular/compiler';
+
+
+@Injectable({
+    providedIn:'root'
+})
+export class AuthService {
+
+    private token = null
+
+    constructor(private http:HttpClient) {
+    }
+    
+    register() {
+        
+    }
+    
+    login(user:User): Observable<{token:string}> {
+        return this.http.post<{token:string}>('/api/auth/login',user)
+        .pipe(
+            tap(
+                ({token})=> {
+                    localStorage.setItem('auth-token',token)
+                }
+            )
+        )
+    }
+
+    setToken(token:string) {
+        this.token = token
+    }
+
+    getToken():string {
+        return this.token
+    }
+
+    isAuthenticated(): boolean {
+        return !!this.token
+    }
+
+    logout() {
+        this.setToken(null)
+        localStorage.clear()
+    }
+
+    
+}
